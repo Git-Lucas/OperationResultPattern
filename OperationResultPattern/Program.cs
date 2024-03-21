78b540f64b1abfe22607e55b7f3f0cdaa773d279
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-03-21 00:18:33 -0300

    [ORP] - feat: Adicionado 'switch expression' para adicionar um código HTTP conforme propriedade do objeto 'Result' (por meio do 'Pattern Matching')

--- OperationResultPattern/Program.cs
+++ OperationResultPattern/Program.cs
@@ -2,6 +2,7 @@
 using OperationResultPattern.DTOs;
 using OperationResultPattern.Enums;
 using OperationResultPattern.UseCase;
+using System.Net;
 
 GenerateInvoiceUseCase generateInvoice = new();
 InvoiceRequest invoiceRequestOutsideOfHours = new(1, new TimeOnly(00, 05), [Items.MargheritaPizza]);
@@ -9,10 +10,20 @@ InvoiceRequest invoiceRequestItemUnavailable = new(1, new TimeOnly(18, 05), [Ite
 InvoiceRequest invoiceRequestValid = new(1, new TimeOnly(22, 00), [Items.MargheritaPizza]);
 
 Result generateInvoiceResult = generateInvoice.Execute(invoiceRequestOutsideOfHours);
-Console.WriteLine(generateInvoiceResult);
+Console.WriteLine(DisplayWithHttpCode(generateInvoiceResult));
 
 generateInvoiceResult = generateInvoice.Execute(invoiceRequestItemUnavailable);
-Console.WriteLine(generateInvoiceResult);
+Console.WriteLine(DisplayWithHttpCode(generateInvoiceResult));
 
 generateInvoiceResult = generateInvoice.Execute(invoiceRequestValid);
-Console.WriteLine(generateInvoiceResult);
+Console.WriteLine(DisplayWithHttpCode(generateInvoiceResult));
+
+static string DisplayWithHttpCode(Result generateInvoiceResult)
+{
+    return generateInvoiceResult switch
+    {
+        { IsSuccess: true } => $"HttpCode = {(int)HttpStatusCode.OK} {generateInvoiceResult}",
+        { IsSuccess: false } => $"HttpCode = {(int)HttpStatusCode.BadRequest} {generateInvoiceResult}",
+        _ => $"HttpCode = {(int)HttpStatusCode.InternalServerError} Unknown error."
+    };
+}
